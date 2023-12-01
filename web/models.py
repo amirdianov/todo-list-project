3d6e0d0232e02981f3d7526eb16753e2915d703f
diyanovamir <amirdianov@gmail.com>
2023-12-01 18:02:38 +0300

    + hello page

--- web/models.py
+++ web/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.contrib.auth.models import make_password
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin
 
 from web.enums import Role
 
@@ -16,12 +16,10 @@ class UserManager(DjangoUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_superuser", True)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, role=Role.admin, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -43,12 +41,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 # Create your models here.
 class TypeTask(models.Model):
     title = models.CharField(max_length=128, verbose_name='Название')
-    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Создатель типа задачи')
+    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL,
+                                     verbose_name='Создатель типа задачи')
 
 
 class TaskList(models.Model):
     title = models.CharField(max_length=128, verbose_name='Заголовок списка')
-    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Создатель списка задач')
+    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL,
+                                     verbose_name='Создатель списка задач')
 
 
 class TodoTask(models.Model):
@@ -58,6 +58,9 @@ class TodoTask(models.Model):
     end_date = models.DateTimeField(verbose_name='Дата конца', null=True, blank=True)
     notify_at = models.DateTimeField(verbose_name='Время отправки уведомления', null=True, blank=True)
     task_type = models.ManyToManyField(TypeTask, verbose_name='Тип задачи', null=True, blank=True)
-    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Создатель задачи')
-    assigned_user = models.ManyToManyField(User, null=True, blank=True, verbose_name='Назначенный сотрудник', related_name='assigned_users')
-    todo_task = models.ForeignKey(TaskList, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Список задач')
+    created_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL,
+                                     verbose_name='Создатель задачи')
+    assigned_user = models.ManyToManyField(User, null=True, blank=True, verbose_name='Назначенный сотрудник',
+                                           related_name='assigned_users')
+    todo_task = models.ForeignKey(TaskList, null=True, blank=True, on_delete=models.SET_NULL,
+                                  verbose_name='Список задач')
