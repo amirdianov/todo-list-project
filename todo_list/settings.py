a32f91422da2ce48ddd178103c2fca6c7b6841fc
diyanovamir <amirdianov@gmail.com>
2023-12-12 11:53:40 +0300

    + sql mid

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "todo_list.middlewares.SqlPrintingMiddleware"
 ]
 
 ROOT_URLCONF = "todo_list.urls"
